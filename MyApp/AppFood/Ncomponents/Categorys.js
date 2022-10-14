32f5cc76ff3f08d292de89afe70842388b7a4071
bapnam <ndnam1411@gmail.com>
2022-10-14 18:45:17 +0700

    add

---
+++ MyApp/AppFood/Ncomponents/Categorys.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { Text, View, TouchableOpacity } from 'react-native';
+
+export default function Categorys(){
+    return (
+        <View>
+            <Text>Category</Text>
+        </View>
+    );
+}
