d9b2fd09543a56b085fa2c0578fc1223b988ac52
bapnam <ndnam1411@gmail.com>
2022-11-16 16:43:52 +0700

    commit

--- MyApp_v1/AppFood/App.js
+++ MyApp_v1/AppFood/App.js
@@ -3,10 +3,35 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Home from './Nscreens/Home';
 import HeaderTab from './Ncomponents/HeaderTab';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+
+  const [SP, setSP] = useState([])
+
+  const getSP = async() =>{
+    await axios.get('http://localhost:9000/v1/sanpham/getall')
+    .then(res=>{
+      setSP(res.data);
+      console.log(res.data);
+    })
+  }
+
+  useEffect(()=>{
+    getSP();
+    console.log('SP1', SP);
+  },[]);
+
+  console.log('SP2', SP);
+
   return (
-    <Home />
+    <View>
+      {SP.map((sp)=>
+      <Text>
+        {SP[0].TenSanPham}
+      </Text>)}
+    </View>
   );
 }
 
