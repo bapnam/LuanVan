046a8ff63b94356e8919cfe4dfcc957cc866b886
bapnam <ndnam1411@gmail.com>
2022-11-30 13:02:31 +0700

    commit

--- MyApp_v1/BookStore/backend/routes/KhachHang.Routes.js
+++
@@ -1,8 +0,0 @@
-const khachHangController = require("../controllers/KhachHangController");
-
-const router = require("express").Router();
-
-// ADD KHACH HANG
-router.post("/add", khachHangController.addKhachHang);
-
-module.exports = router;
